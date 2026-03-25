387cecf273dd6e2392e805895af7340e47f92b99
Marwan Mohamed Zidane <mmzidane101@gmail.com>
2026-03-25 18:22:01 +0200

    Update start_date in DAG to January 1, 2024

--- airflow/dags/postgre-to-bq-airbyte.py
+++ airflow/dags/postgre-to-bq-airbyte.py
@@ -1,11 +1,12 @@
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
+from datetime import datetime
 
 with DAG(dag_id='trigger_airbyte_job_example',
          default_args={'owner': 'airflow'},
          schedule='@daily',
-         start_date=days_ago(1)
+         start_date=datetime(2024, 1, 1)
     ) as dag:
 
     money_to_json = AirbyteTriggerSyncOperator(
