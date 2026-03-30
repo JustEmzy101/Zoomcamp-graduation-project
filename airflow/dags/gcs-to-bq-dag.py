82d4f65fcca139e7b5fac7c820a33e8629a269da
Marwan Mohamed Zidane <mmzidane101@gmail.com>
2026-03-30 18:24:48 +0200

    Refactor DAG to use local YAML and EmptyOperator

--- airflow/dags/gcs-to-bq-dag.py
+++ airflow/dags/gcs-to-bq-dag.py
@@ -1,51 +1,35 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.providers.standard.operators.empty import EmptyOperator
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from datetime import datetime
-import requests
 
-def fetch_spark_yaml(**kwargs):
-    """Pull the latest SparkApplication YAML from GitHub"""
-    url = "https://raw.githubusercontent.com/JustEmzy101/Zoomcamp-graduation-project/main/spark_apps/gcs_to_bq.yaml"
-    response = requests.get(url)
-    response.raise_for_status()
-    
-    yaml_content = response.text
-    
-    # Make the job name unique per DAG run (very important!)
-    yaml_content = yaml_content.replace(
-        "name: gcs-to-bq",
-        f"name: gcs-to-bq-v1"
-    )
-    
-    # Push the full YAML to XCom so the next task can use it
-    kwargs['ti'].xcom_push(key='spark_yaml', value=yaml_content)
+
+REPO_ROOT = os.path.join(os.path.dirname(__file__), "../..")
+with open(os.path.join(REPO_ROOT, "spark_apps/using_spark.yaml")) as f:
+    spark_app = yaml.safe_load(f)
+
 
 with DAG(
-    dag_id="gcs_to_bq_github_fresh",
+    dag_id="spark_using_kubeflow_operator",
     schedule=None,
     start_date=datetime(2025, 1, 1),
     catchup=False,
-    tags=["spark", "gcs", "bigquery", "github"],
+    tags=["kubeflow","spark", "gcs", "bigquery", "github"],
     max_active_runs=1,
 ) as dag:
 
-    # Task 1: Pull latest YAML from your GitHub repo
-    fetch_yaml = PythonOperator(
-        task_id="fetch_latest_yaml_from_github",
-        python_callable=fetch_spark_yaml,
-    )
+    
+    starting = EmptyOperator(task_id="start")
 
     # Task 2: Submit to Spark Kubernetes Operator
     submit_spark_job = SparkKubernetesOperator(
-        task_id="submit_gcs_to_bq",
+        task_id="new_submit_gcs_to_bq",
         namespace="spark",
-        application_file="{{ ti.xcom_pull(task_ids='fetch_latest_yaml_from_github', key='spark_yaml') }}",
+        application_file=yaml.dump(spark_app),
         kubernetes_conn_id="kubernetes_default",
-        do_xcom_push=True,
         get_logs=True,
         delete_on_termination=True,
-        is_delete_operator_pod=True,
+
     )
 
     fetch_yaml >> submit_spark_job
