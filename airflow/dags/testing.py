36883d405c8a2b49dbd4e3c334af7290b81ed638
Marwan Mohamed Zidane <mmzidane101@gmail.com>
2026-04-08 12:42:26 +0200

    Update testing.py

--- airflow/dags/testing.py
+++ airflow/dags/testing.py
@@ -170,7 +170,7 @@ with DAG(
 
     validate_schema = PythonOperator(
         task_id="validate_data_schema_before_triggering_sync",
-        python_callable=validate_schema(),
+        python_callable=validate_schema,
     )
 
 #    trigger_sync = PythonOperator(
