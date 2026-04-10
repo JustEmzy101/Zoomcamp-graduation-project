c5ec58fcc582e2383513ce29ad7641dadf95c42a
Marwan Mohamed Zidane <mmzidane101@gmail.com>
2026-04-10 20:05:15 +0200

    Update testing_dbt.py

--- airflow/dags/testing_dbt.py
+++ airflow/dags/testing_dbt.py
@@ -1,11 +1,11 @@
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
-from airflow.utils.dates import days_ago
+from datetime import datetime
 from kubernetes.client import models as k8s
 
 default_args = {
     'owner': 'airflow',
-    'start_date': days_ago(1),
+    'start_date': datetime(2024, 1, 1),
 }
 
 with DAG(
