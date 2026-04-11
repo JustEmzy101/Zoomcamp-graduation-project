f2cd48df532365221d1a665000c273260ab71744
Marwan Mohamed Zidane <mmzidane101@gmail.com>
2026-04-11 14:16:45 +0200

    Create int_accounts_deduplicated.sql

---
+++ dbt/models/intermediate/int_accounts_deduplicated.sql
@@ -0,0 +1,20 @@
+with ranked as (
+    select
+        *,
+        row_number() over (
+            partition by account_id
+            order by _ab_cdc_lsn desc, _airbyte_extracted_at desc
+        ) as row_num
+    from {{ ref('stg_accounts') }}
+    where _ab_cdc_deleted_at is null  -- exclude soft-deleted records (if desired)
+)
+
+select
+    account_id,
+    user_id,
+    balance,
+    currency,
+    _ab_cdc_updated_at as last_updated_at,
+    _airbyte_extracted_at as ingested_at
+from ranked
+where row_num = 1
